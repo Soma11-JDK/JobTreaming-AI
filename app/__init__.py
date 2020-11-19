b9c4d379fe54d38ceeacf33f228adccea056e18d
Ubuntu <ubuntu@ip-172-31-41-43.ap-northeast-2.compute.internal>
2020-11-19 01:46:29 +0000

    CORS 설정 추가

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from app import SimilarityAnalyzer
 
 app = Flask(__name__)
+CORS(app)
 app.config['JSON_AS_ASCII'] = False
 analyzer = SimilarityAnalyzer.SimilarityAnalyzer()
 
